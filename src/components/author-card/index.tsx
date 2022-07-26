a99e2f5356fef3542d6b96cc72e3891d56192b0d
cosart <1398675906@qq.com>
2022-07-26 11:07:16 +0800

    优化用户卡片

--- src/components/author-card/index.tsx
+++ src/components/author-card/index.tsx
@@ -9,6 +9,14 @@ const AuthorCard = () => {
       <div className={style['personal-info-wrapper']}>
         <Image src={Avartar} width={100} height={100} alt="头像" />
         <h3 className={style['name']}>蔡夏柠</h3>
+        <p className={style['desc']}>hhhhhhhh</p>
+        <button>
+          <SvgGo
+            icon="github"
+            style={{ width: '1.4rem', height: '1.4rem', verticalAlign: 'bottom', marginRight: '5px' }}
+          />
+          Follow Me
+        </button>
         <div className={style['num']}>
           <div>
             <h3>142</h3>
