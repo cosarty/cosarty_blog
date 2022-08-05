69fb9ac9d18efd237faa6003a117c4e240fe0864
cosart <1398675906@qq.com>
2022-08-06 00:32:40 +0800

    侧边栏点击隐藏

--- src/global/nav-bar/index.tsx
+++ src/global/nav-bar/index.tsx
@@ -1,11 +1,13 @@
 import style from './navbar.module.scss'
 import NvaWidget from './nva-widget'
 import { MENU_CONFIG } from '@/constants/nav-link'
-import { FC, useState } from 'react'
+import { FC, MouseEvent, useState } from 'react'
 
 const NavBar: FC<{ count: noteNumType }> = ({ count }) => {
   const [showSideBar, setShowSideBar] = useState(false)
-
+  const maskHandle = (e: MouseEvent<HTMLDivElement>) => {
+    setShowSideBar(false)
+  }
   return (
     <>
       <header className={style['navbar-box']}>
@@ -27,6 +29,14 @@ const NavBar: FC<{ count: noteNumType }> = ({ count }) => {
           <NvaWidget.Links count={count} conf={MENU_CONFIG} show={showSideBar} />
         </nav>
       </header>
+      {showSideBar && (
+        <div
+          className={style['side-mask']}
+          onClick={(e) => {
+            maskHandle(e)
+          }}
+        ></div>
+      )}
     </>
   )
 }
